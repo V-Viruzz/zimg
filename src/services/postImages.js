95c99352c9345c30a35e95e56911530c2a6da9a2
viruz <nahuelsinka@gmail.com>
2023-04-09 19:58:51 -0300

    fix 4

--- src/services/postImages.js
+++ src/services/postImages.js
@@ -1,6 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL || process.env.VITE_API_URL
 
-console.log(API_URL)
 const postImages = async (formData) => {
   try {
     const response = await fetch(`${API_URL}/upload`, {
